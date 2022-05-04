6c59720fb6168dc0d445cd7baf295ac8dac34605
Fang-Wen Lee <66965736+fang-wenlee@users.noreply.github.com>
2022-05-04 16:45:14 -0700

    Looping array

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import "./styles.css";
 import Counter from "./components/counter";
-import React, { useState } from "react";
+import React from "react";
 import { useSelector } from "react-redux";
 
 function App() {
   const count = useSelector((state) => state.counter.count);
+  const voters = [
+    "Fang-wen",
+    "Hsi-Wen",
+    "Bob Smith",
+    "Stephanie Foo",
+    "Pohan Lin",
+    "John Ho"
+  ];
   return (
     <div className="App">
       <h1>Total votes: {count}</h1>
-
-      <Counter name="fang" />
-      <Counter name="Dave" />
+      {voters.map((voter, i) => (
+        <Counter key={i} name={voter} />
+      ))}
     </div>
   );
 }
--- src/components/counter.js
+++ src/components/counter.js
@@ -3,19 +3,26 @@ import { useDispatch } from "react-redux";
 import { increment, decrement } from "../redux/ducks/counter";
 
 function Counter(props) {
+  const [count, setCount] = useState(0);
   const dispatch = useDispatch();
 
   const handleIncrement = () => {
     dispatch(increment());
+    setCount(count + 1);
   };
 
   const handleDecrement = () => {
     dispatch(decrement());
+    setCount(count - 1);
   };
 
   return (
-    <div style={{ backgroundColor: "grey", margin: "10px", padding: "10px" }}>
-      <h3>{props.name}</h3>
+    <div
+      style={{ backgroundColor: "lightgrey", margin: "10px", padding: "10px" }}
+    >
+      <h3>
+        {props.name} vote: {count}
+      </h3>
       <button onClick={handleIncrement}>+</button>
       <button onClick={handleDecrement}>-</button>
     </div>
